f59bd51f0c780c462a967f16dbb137eef9367f05
rajesh <coding3601@gmail.com>
2020-12-23 22:23:05 -0800

    updated version 2.0

--- public/game.js
+++ public/game.js
@@ -7,13 +7,6 @@ function increaseScore(score){
   document.querySelector('.score').innerHTML = score;
 }
 
-// gameover
-const gameOver = ()=>{
-  localStorage.setItem('cScore',JSON.stringify(curScore));
-  document.querySelector('.check').style.display = "none";
-  document.querySelector('.gameOver').style.display = "block";
-}
-
 
 // shuffling question
 function shuffleArray(array) { 
@@ -40,6 +33,17 @@ const checkWhoAmI = ()=>{
   }
 }
 
+// checking  whether any radio button is checked or not
+const checked = ()=>{
+  const radio = document.getElementsByName('option');
+  for(let i=0;i<radio.length;i++){
+    if(radio[i].checked){
+      return true;
+    }
+  }
+  return false;
+}
+
 // enable or disable radio button
 const disable = (val)=>{
   document.querySelector('#op1').disabled = val;
@@ -54,10 +58,14 @@ const game = data=>{
   document.querySelector('.check').style.pointerEvents = 'auto';
   document.querySelector('.next').style.pointerEvents = 'none';
 
-  document.querySelector('.number').innerHTML = ind+1;
-  document.querySelector('.score').innerHTML = curScore;
+  document.querySelector('.nameDisplay .name').innerHTML = `<h2>${JSON.parse(localStorage.getItem('name'))}'s Game</h2>`;
+  document.querySelector('.nameDisplay .score').innerHTML = `
+  <h2>Score</h2>
+  <span>${curScore}</span>
+  `;
+
   document.querySelector('p.question').innerText = data[ind].question;
-  let tag = `<span class="tag">${ind+1}/10</span>`;
+  let tag = `<span class="tag">${ind+1}/25</span>`;
   document.querySelector('p.question').innerHTML += tag;
   let options = [data[ind].correct_answer,data[ind].incorrect_answers[0],data[ind].incorrect_answers[1],data[ind].incorrect_answers[2]];
 
@@ -87,88 +95,109 @@ const loader = (opac,dis)=>{
 // event listener for check button
 document.querySelector('.check').addEventListener('click',(e)=>{
 
-  // displaying loader
-  loader('0.4','block');
+  if(checked()){
 
-  setTimeout(()=>{
-    // removing loader
-    loader('1','none');
-      
-    const op1 = document.querySelector('#op1');
-    const op2 = document.querySelector('#op2');
-    const op3 = document.querySelector('#op3');
-    const op4 = document.querySelector('#op4');
-    const cAns = qns[ind].correct_answer;
+    // displaying loader
+    loader('0.4','block');
 
-    if(yourAnswer === cAnsOption){
-      document.getElementsByName('option')[yourAnswer].parentElement.classList.remove('select');
-      document.getElementsByName('option')[yourAnswer].parentElement.classList.add('correct');
-      // disable check
-      document.querySelector('.check').style.pointerEvents = "none";
-      // enable next
-      document.querySelector('.next').style.pointerEvents = "auto";
+    setTimeout(()=>{
+      // removing loader
+      loader('1','none');
+        
+      const op1 = document.querySelector('#op1');
+      const op2 = document.querySelector('#op2');
+      const op3 = document.querySelector('#op3');
+      const op4 = document.querySelector('#op4');
+      const cAns = qns[ind].correct_answer;
 
-      ind++,curScore++;
+      if(yourAnswer === cAnsOption){
+        document.getElementsByName('option')[yourAnswer].parentElement.classList.remove('select');
+        document.getElementsByName('option')[yourAnswer].parentElement.classList.add('correct');
+        // disable check
+        document.querySelector('.check').style.pointerEvents = "none";
+        // enable next
+        document.querySelector('.next').style.pointerEvents = "auto";
 
-      if(curScore === 10){
+        ind++,curScore++;
+
+        if(curScore === 10){
+          localStorage.setItem('cScore',JSON.stringify(curScore));
+          window.location.href = 'playAgain.html';
+        }
+
+      }else{
+        // For your wrong Answer
+        document.getElementsByName('option')[yourAnswer].parentElement.classList.remove('select');
+        document.getElementsByName('option')[yourAnswer].parentElement.classList.add('wrong');
+
+        // For the correct Answer
+        document.getElementsByName('option')[cAnsOption].parentElement.classList.remove('select');
+        document.getElementsByName('option')[cAnsOption].parentElement.classList.add('correct');
+
+        // storing player's score
         localStorage.setItem('cScore',JSON.stringify(curScore));
-        window.location.href = 'playAgain.html';
+
+        // disable check and next 
+        document.querySelector('.check').style.display = "none";
+        document.querySelector('.next').style.display = "none";
+
+        // enable gameOver
+        document.querySelector('.gameOver').style.display = "block";
       }
 
-    }else{
-      // For your wrong Answer
-      document.getElementsByName('option')[yourAnswer].parentElement.classList.remove('select');
-      document.getElementsByName('option')[yourAnswer].parentElement.classList.add('wrong');
+      // disable all radio button after check
+      disable('true');
 
-      // For the correct Answer
-      document.getElementsByName('option')[cAnsOption].parentElement.classList.remove('select');
-      document.getElementsByName('option')[cAnsOption].parentElement.classList.add('correct');
+      // button color swap
+      e.target.classList.remove('clicked');
+      document.querySelector('.next').classList.add('clicked');
 
-      // storing player's score
-      localStorage.setItem('cScore',JSON.stringify(curScore));
+    },1500);
 
-      // disable check and next 
-      document.querySelector('.check').style.display = "none";
-      document.querySelector('.next').style.display = "none";
-
-      // enable gameOver
-      document.querySelector('.gameOver').style.display = "block";
-    }
-
-    // disable all radio button after check
-    disable('true');
-  },1500);
+  }else{
+    alert('Please, make sure that you have selected any option!');
+  }
   
 });
 
 // event listener for next question button
 document.querySelector('.next').addEventListener('click',(e)=>{
+  e.target.classList.remove('clicked');
+  document.querySelector('.check').classList.add('clicked');
   increaseScore(curScore);
   disable('false');
   game(qns);
 })
 
-// Fetching Data
-const call = ()=>{
-  if(!(JSON.parse(localStorage.getItem('token')))){
-    fetch('https://opentdb.com/api_token.php?command=request')
+const getToken = ()=>{
+  fetch('https://opentdb.com/api_token.php?command=request')
     .then(res => res.json())
     .then(data => {
       localStorage.setItem('token',JSON.stringify(data.token));
-      fetch(`https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple&token=${data.token}`)
+      fetch(`https://opentdb.com/api.php?amount=25&difficulty=easy&type=multiple&token=${data.token}`)
       .then(res =>  res.json())
       .then(data => {
         qns = data.results;
         game(qns);
       });
     });
+}
+
+// Fetching Data
+const call = ()=>{
+  if(!(JSON.parse(localStorage.getItem('token')))){
+    getToken();
   }else{
     let token = JSON.parse(localStorage.getItem('token'));
-    fetch(`https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=multiple&token=${token}`)
+    fetch(`https://opentdb.com/api.php?amount=25&difficulty=easy&type=multiple&token=${token}`)
     .then(res =>  res.json())
     .then(data => {
-      qns = data.results;
-      game(qns);
+      if(data.results.length){
+        qns = data.results;
+        game(qns);
+      }else{
+        getToken();
+      }
     });
   }
 }
